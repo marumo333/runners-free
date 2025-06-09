2e0b84c9ebbd9db95af8f886307ca933b4a5bae4
marumo333 <seito5026junior@icloud.com>
2025-06-09 12:56:10 +0900

    add:Headerファイルを追加

---
+++ app/components/Header.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState,useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import { useSetRecoilState } from "recoil";
+import { isMobileState } from "./atoms";
+import MobileComponent from "./MobileComponent";
+import PCComponent from "./PCComponent";
+
+export default function Header({ children }: { children: React.ReactNode }) {
+    const [isClient, setIsClient] = useState(false);
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
+    const setIsMobile = useSetRecoilState(isMobileState);
+  
+    useEffect(() => {
+      setIsClient(true);
+    }, []);
+  
+    useEffect(() => {
+      if (isClient) {
+        setIsMobile(isMobile);
+      }
+    }, [isMobile, isClient]);
+  
+    if (!isClient) return null;
+  
+    return (
+      <>
+        {isMobile ? (
+          <MobileComponent className="lg:hidden" />
+        ) : (
+          <PCComponent className="max-lg:hidden" />
+        )}
+        <main className="pt-16">{children}</main>
+      </>
+    );
+  }
+  
