d7289c732c03aa56add0d5e08c38a39d81dba710
marumo333 <seito5026junior@icloud.com>
2025-06-18 19:02:02 +0900

    商品一覧ページを追加

---
+++ app/post/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import ShopImage from "./ShopImage";
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Private() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  if (!isClient) {
+    return <h1>読み込み中....</h1>;
+  }
+  return (
+    <>
+      <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900">商品一覧</h1>
+          <p className="mb-4">
+            *表示された画像は5分でリンクが不可になります。
+            <br />
+            再度アクサスした場合はリロードボタンを押してください。
+          </p>
+          <div className="flex-1 w-full flex flex-col items-center">
+            <ShopImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
