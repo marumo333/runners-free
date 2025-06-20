7c117bd10e8729bf67fa4c7b9e2964def0ef7155
marumo333 <seito5026junior@icloud.com>
2025-06-20 15:17:30 +0900

    登録フォームを作成

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,17 +1,51 @@
-'use client'
-import {useState} from "react";
+"use client";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/utils/supabase/supabase"
+import { supabase } from "@/utils/supabase/supabase";
 
-export default function Register (){
-    const [email,setEmail] = useState("")
-    const [password,setPassWord] = useState("")
-    const [role,setRole] = useState<"admin"|"customer">("customer")
-    const router = useRouter();
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState<"admin" | "customer">("customer");
+  const router = useRouter();
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { error } = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email, password, role }),
+    }).then((res) => res.json());
+    if (!error) {
+      router.replace("/Login");
+    } else {
+      alert(error);
+    }
+  };
 
-    
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password (≥8)"
+          minLength={8}
+          required
+        />
+        <select value={role} onChange={(e) => setRole(e.target.value as any)}>
+          <option value="freelancer">フリーランス</option>
+          <option value="customer">お客様</option>
+        </select>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 }
