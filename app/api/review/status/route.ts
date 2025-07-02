39cab85d2c9c7734b47feaa0ca472cd161bcb72e
marumo333 <seito5026junior@icloud.com>
2025-07-02 23:07:08 +0900

    不要な宣言文を削除

--- app/api/review/status/route.ts
+++ app/api/review/status/route.ts
@@ -1,7 +1,7 @@
 // app/api/review/status/route.ts
 import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { cookies} from "next/headers";
 
 export async function GET() {
   const supabase = createRouteHandlerClient({ cookies: async () => cookies() })
