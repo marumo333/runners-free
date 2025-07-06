7258ff391e2ea56770e7c5f5e41b888be95f2678
marumo333 <seito5026junior@icloud.com>
2025-07-06 22:10:10 +0900

    deeplのPOSTAPIを追加

---
+++ libs/deepl.tsx
@@ -0,0 +1,20 @@
+export async function Translator(text: string, target: string) {
+    const res = await fetch("/api/translate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        target_lang: target,
+      }),
+    });
+    
+    
+      if (!res.ok) {
+        const err = await res.text();
+        throw new Error(`翻訳失敗: ${res.status} - ${err}`);
+      }
+    
+      return await res.json(); 
+    }
