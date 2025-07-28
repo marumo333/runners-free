93cf0125894f86dc6c37d292846adbdd9d80633c
marumo333 <seito5026junior@icloud.com>
2025-07-28 19:50:42 +0900

    add:カート表示をlayout全体に反映させるためにcontextを設定

--- app/context/page.tsx
+++ app/context/page.tsx
@@ -1,7 +1,60 @@
+"use client"
 import React, { createContext, useContext, useState } from "react";
-export default function Context(){
+import type { CartItem,ShopPost } from "@/app/types/cart";
+
+type CartContextType = {
+    cart: CartItem[];
+    addCart: (post: ShopPost) => void;
+    removeCart: (post: ShopPost) => void;
+}
+
+const CartContext = createContext<CartContextType|undefined>(undefined)
+export default function Context({ children }: { children: React.ReactNode }){
+    const [cart,setCart] = useState<CartItem[]>([])
+
+    const addCart = (post: ShopPost) => {
+        setCart((prev: CartItem[]) => {
+            const index = prev.findIndex((item) => item.id === post.id);
+            if (index !== -1) {
+                const next = [...prev];
+                next[index].quantity += 1;
+                return next;
+            }
+            return [...prev, { ...post, quantity: 1 }];
+        });
+    };
+
+    const removeCart = (post:ShopPost) =>{
+        setCart((prev: CartItem[]) => {
+            const index = prev.findIndex((item) => item.id === post.id);
+            if (index === -1) return prev;
+
+            const next = [...prev];
+            if(next[index].quantity > 1){
+                next[index].quantity  -= 1;
+                return next;
+            }
+            next.splice(index,1);
+            return next;
+        });
+    }
+
     
 
-    return(<>
-    </>)
-}
+
+    
+    return(
+    <>
+    <CartContext.Provider value={{ cart, addCart, removeCart }}>
+      {children}
+    </CartContext.Provider>
+    </>
+    );
+};
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
